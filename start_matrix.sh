ac9ae5ad24836c2c1371b5f122ff0122ee456fdb
Carlin-Moker <juanmartinezlara1980@users.noreply.github.com>
2026-05-31 03:07:12 -0600

    feat: core(red62) - inicialización de autarquía total en 3 nodos independientes

---
+++ start_matrix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export ECOSYSTEM_IDENTIFIER="CJB"
+export NATIVE_TOKEN="CJ"
+export LIQUIDITY_TOKEN="CGL"
+export NODO_ALFA="127.0.0.1:5001"
+export NODO_BETA="127.0.0.1:5002"
+export NODO_GAMMA="127.0.0.1:5003"
+
+echo "[+] Estructura trilateral cargada bajo protección divina."
