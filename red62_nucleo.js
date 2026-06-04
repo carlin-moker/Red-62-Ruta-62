76b54eece9ad8598fe00f7de60bb1c036f485684
Carlin-Moker <juanmartinezlara1980@users.noreply.github.com>
2026-06-03 18:05:42 -0600

    Update core constructor and translate interface to English

--- red62_nucleo.js
+++
@@ -1,6 +0,0 @@
-// Ruta raíz principal que soluciona el "Cannot GET /"
-app.get('/', (req, res) => {
-    // Te redirige en caliente al gateway local con tu CID consolidado
-    res.redirect('http://127.0.0.1:8080/ipfs/QmUf9uuWpPQjQDcLaTE39jQLED8zScmyvXyhwFVYzj69sB');
-});
-
